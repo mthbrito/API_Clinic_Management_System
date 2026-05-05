54b8bf95f7afef45a65731171a3eb3e624d1af53
Matheus Brito <matheus.sbrito14@gmail.com>
2026-05-05 19:55:44 -0300

    first commit

---
+++ src/main/java/ifpb/api_clinic_management_system/ApiClinicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ifpb.api_clinic_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifpb/api_clinic_management_system/ApiClinicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ifpb.api_clinic_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
