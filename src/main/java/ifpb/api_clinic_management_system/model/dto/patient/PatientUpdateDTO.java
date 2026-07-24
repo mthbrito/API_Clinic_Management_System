ed14221c6d033fe9df1df77830b80da6c5596195
Matheus Brito <matheus.sbrito14@gmail.com>
2026-07-24 18:40:59 -0300

    config: Configuring application.properties

--- src/main/java/ifpb/api_clinic_management_system/model/dto/patient/PatientUpdateDTO.java
+++ src/main/java/ifpb/api_clinic_management_system/model/dto/patient/PatientUpdateDTO.java
@@ -1,6 +1,6 @@
 package ifpb.api_clinic_management_system.model.dto.patient;
 
-import ifpb.api_clinic_management_system.model.dto.AddressDTO;
+import ifpb.api_clinic_management_system.model.dto.address.AddressDTO;
 import ifpb.api_clinic_management_system.model.enumeration.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
